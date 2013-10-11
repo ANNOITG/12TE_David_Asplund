25d7f094f01f964effe1bd38c921f28477372681
lugapwns <david.asplund@hotmail.com>
2013-10-11 12:13:12 +0200

    La up uppgift 11.1
    Är inte helt klar. Work in progress.

---
+++ Programmering 1/11.1/11.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._1
+{
+    class Program
+    {
+        static void Hello(string namn)
+        {
+            Console.WriteLine("Hello " + namn + "!");
+        }
+        static void Main(string[] args)
+        {
+            Hello("");
+            Console.ReadKey();
+        }
+    }
+}
